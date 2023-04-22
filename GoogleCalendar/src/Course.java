e116191905f2d5fa754eb9f99487b013c1ce1f6e
austintnguyen <atn038@ucsd.edu>
2023-04-22 13:46:21 -0700

    Update Course.java

---
+++ GoogleCalendar/src/Course.java
@@ -0,0 +1,12 @@
+public class Course {
+    private String name;
+    private String startTime;
+    private String endTime;
+    private String location;
+
+    public Course(String name, String startTime, String endTime, String location){
+
+        
+
+    }
+}
