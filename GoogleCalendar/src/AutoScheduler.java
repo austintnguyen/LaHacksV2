ac987c075f35afb54b4f6a1af6c0869a356d587f
austinyu12 <ausno5@gmail.com>
2023-04-23 02:34:12 -0700

    renamed GUI to AutoScheduler

--- GoogleCalendar/src/AutoScheduler.java
+++ GoogleCalendar/src/AutoScheduler.java
@@ -11,13 +11,30 @@ public class AutoScheduler extends JFrame {
     private JButton submitButton;
     public String startDate;
     public int[] numClasses;
+    private int padding = 10;
 
     private void createDate(JPanel p) {
         JPanel datePanel = new JPanel();
-        datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.Y_AXIS));
-        startDateI = new JTextArea(10, 10);
+        //datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.Y_AXIS)); //might remove this
+        datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.PAGE_AXIS));
+        datePanel.add(Box.createVerticalGlue());
+
+        //create label
+        JLabel dateLabel = new JLabel("Enter the date on which instruction began in YYYYMMDD format: ");
+        datePanel.add(dateLabel);
+        //datePanel.add(Box.createVerticalGlue());
+        //dateLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        //create start date text box
+        startDateI = new JTextArea(5, 10);
         startDateI.setLineWrap(true);
         startDateI.setWrapStyleWord(true);
+        datePanel.add(startDateI);
+        //datePanel.add(Box.createVerticalGlue());
+
+        //datePanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        datePanel.setBorder(BorderFactory.createEmptyBorder(padding, padding, padding, padding));
         p.add(datePanel);
     }
 
@@ -62,6 +79,7 @@ public class AutoScheduler extends JFrame {
             classPanel.add(classes.get(i));
         }
 
+        classPanel.setBorder(BorderFactory.createEmptyBorder(padding, padding, padding, padding));
         p.add(classPanel);
     }
     
@@ -72,49 +90,42 @@ public class AutoScheduler extends JFrame {
 
         //create label
         JLabel label = new JLabel("Input Schedule");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
         bodyPanel.add(label);
         //bodyPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
         //create schedule text box
-        scheduleI = new JTextArea(100, 100);
+        scheduleI = new JTextArea(80, 40);
         scheduleI.setLineWrap(true);
         scheduleI.setWrapStyleWord(true);
         //bodyPanel.setMaximumSize(bodyPanel.getPreferredSize());
         bodyPanel.add(new JScrollPane(scheduleI));
-        bodyPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        //bodyPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
         //create button
         submitButton = new JButton("Submit");
+        submitButton.setHorizontalAlignment(SwingConstants.CENTER);
         submitButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 InputOutput io = new InputOutput();
-<<<<<<< Updated upstream:GoogleCalendar/src/GUI.java
-                io.storeData(scheduleI, startDateI, daysCombo, startDate, numClasses);
-=======
                 //create file
                 io.createFile(scheduleI.getText());
                 //parse data
+                
                 //getNumDays
                 //io.storeData(scheduleI, startDateI, daysCombo, startDate, numClasses);
->>>>>>> Stashed changes:GoogleCalendar/src/AutoScheduler.java
                 //call methods?
             }
         });
         bodyPanel.add(submitButton);
+        bodyPanel.setBorder(BorderFactory.createEmptyBorder(padding, padding, padding, padding));
+
         p.add(bodyPanel);
     }
 
-<<<<<<< Updated upstream:GoogleCalendar/src/GUI.java
-    private void createOutput(JPanel p) {
-
-    }
-
-    public GUI() {
-=======
     public AutoScheduler() {
->>>>>>> Stashed changes:GoogleCalendar/src/AutoScheduler.java
         super("AutoScheduler");
-        setSize(400, 800);
+        setSize(1000, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel superPanel = new JPanel();
@@ -123,36 +134,14 @@ public class AutoScheduler extends JFrame {
         createDate(superPanel);
         createNumClasses(superPanel);
         createBody(superPanel);
-        //createOutput(superPanel);
 
         add(superPanel);
-
-        // output box
-        // o = new JTextArea(20, 40);
-        // o.setLineWrap(true);
-        // o.setWrapStyleWord(true);
-        // o.setEditable(false);
-
-        //JPanel inputPanel = new JPanel(new BorderLayout());
-        //inputPanel.add(new JScrollPane(scheduleI), BorderLayout.CENTER);
-        //inputPanel.add(submitButton, BorderLayout.SOUTH);
-
-        // JPanel outputPanel = new JPanel(new BorderLayout());
-        // outputPanel.add(new JScrollPane(outputTextArea), BorderLayout.CENTER);
-
-       //getContentPane().setLayout(new GridLayout(2, 1));
-        //getContentPane().add(inputPanel);
-        //getContentPane().add(outputPanel);
+        
     }
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream:GoogleCalendar/src/GUI.java
-        GUI GUI = new GUI();
-        GUI.setVisible(true);
-=======
         AutoScheduler AutoScheduler = new AutoScheduler();
         AutoScheduler.pack();
         AutoScheduler.setVisible(true);
->>>>>>> Stashed changes:GoogleCalendar/src/AutoScheduler.java
       }
 }
--- GoogleCalendar/src/InputOutput.java
+++ GoogleCalendar/src/InputOutput.java
@@ -8,6 +8,8 @@ import java.awt.event.*;
 public class InputOutput {
     int[] numClasses;
 
+    int[] startDate = {2023,4,3};
+
     public void intro() {
         System.out.println("Copy and input your schedule into the terminal "
         +"(Paste without formatting and end input with the keyword STOP):");
@@ -54,8 +56,6 @@ public class InputOutput {
         return numClasses;
     }
 
-<<<<<<< Updated upstream
-=======
     public int [] getStartDate(){
         return startDate;
     }
@@ -74,30 +74,4 @@ public class InputOutput {
         }
     }
 
->>>>>>> Stashed changes
-    /**
-     * create a file for schedule
-     * stores start date as int
-     * stores num of classes as int[]
-     * 
-     * @param scheduleI the body of text the represents a schedule
-     * @param startDateI the first monday of the week they start classes
-     * @param daysCombo the combo boxes
-     * @return
-     */
-    // public void storeData(JTextArea scheduleI, JTextArea startDateI, 
-    // ArrayList<JComboBox<Integer>> daysCombo, String storeDate, int[] storeNumClasses) {
-    //     String startDate = startDateI.getText();
-    //     System.out.println("startDate is: " + startDate);
-
-    //     // Integer mon = (Integer) monI.getSelectedItem();
-    //     // Integer tue = (Integer) tueI.getSelectedItem();
-    //     // Integer wed = (Integer) wedI.getSelectedItem();
-    //     // Integer thu = (Integer) thuI.getSelectedItem();
-    //     // Integer fri = (Integer) friI.getSelectedItem();
-
-
-    //     //put schedule into file
-
-    // }
 }
