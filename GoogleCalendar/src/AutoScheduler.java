92db4d1d1b5f3d479c26b2e63011edc9c8872113
austinyu12 <ausno5@gmail.com>
2023-04-23 04:58:23 -0700

    clean autograder

--- GoogleCalendar/src/AutoScheduler.java
+++ GoogleCalendar/src/AutoScheduler.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 
 public class AutoScheduler extends JFrame {
     private JTextArea scheduleI, startDateI, o;
-    //private JPanel startDatePanel, classPanel, bodyPanel, outputPanel;
     private ArrayList<JComboBox<Integer>> daysCombo;
     private ArrayList<JPanel> classes;
     private JButton submitButton;
@@ -15,24 +14,18 @@ public class AutoScheduler extends JFrame {
 
     private void createDate(JPanel p) {
         JPanel datePanel = new JPanel();
-        //datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.Y_AXIS)); //might remove this
         datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.PAGE_AXIS));
         datePanel.add(Box.createVerticalGlue());
 
         //create label
         JLabel dateLabel = new JLabel("Enter the date on which instruction began in YYYYMMDD format: ");
         datePanel.add(dateLabel);
-        //datePanel.add(Box.createVerticalGlue());
-        //dateLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         //create start date text box
         startDateI = new JTextArea(5, 10);
         startDateI.setLineWrap(true);
         startDateI.setWrapStyleWord(true);
         datePanel.add(startDateI);
-        //datePanel.add(Box.createVerticalGlue());
-
-        //datePanel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         datePanel.setBorder(BorderFactory.createEmptyBorder(padding, padding, padding, padding));
         p.add(datePanel);
@@ -49,31 +42,16 @@ public class AutoScheduler extends JFrame {
 
         ArrayList<JPanel> classes = new ArrayList<>();
 
-            //initialized combo boxes first
-        // for (int i = 0; i < daysCombo.size(); i++) {
-        //     daysCombo.add(new JComboBox<Integer>(new Integer[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
-        //     daysCombo.get(i).setPreferredSize(new Dimension(10, 10));
-        //     //classPanel.add(daysCombo.get(i));
-        // }
-
         for (int i = 0; i < 5; i++) {
             JPanel panel = new JPanel();
             panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
             panel.add(new JLabel(days[i]));
             daysCombo.add(new JComboBox<Integer>(new Integer[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
             daysCombo.get(i).setPreferredSize(new Dimension(10, 10));
-            //Component glue = Box.createVerticalGlue();
-            //panel.add(glue);
-            //panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
             panel.add(daysCombo.get(i));
             classes.add(panel);
         }
-
-        // for (int i = 0; i < daysCombo.size(); i++) {
-        //     daysCombo.add(new JComboBox<Integer>(new Integer[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
-        //     daysCombo.get(i).setPreferredSize(new Dimension(10, 10));
-        //     classPanel.add(daysCombo.get(i));
-        // }
 
         for (int i = 0; i < 5; i++) {
             classPanel.add(classes.get(i));
@@ -92,15 +70,12 @@ public class AutoScheduler extends JFrame {
         JLabel label = new JLabel("Input Schedule");
         label.setHorizontalAlignment(SwingConstants.CENTER);
         bodyPanel.add(label);
-        //bodyPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
         //create schedule text box
         scheduleI = new JTextArea(80, 40);
         scheduleI.setLineWrap(true);
         scheduleI.setWrapStyleWord(true);
-        //bodyPanel.setMaximumSize(bodyPanel.getPreferredSize());
         bodyPanel.add(new JScrollPane(scheduleI));
-        //bodyPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
         //create button
         submitButton = new JButton("Submit");
@@ -156,9 +131,4 @@ public class AutoScheduler extends JFrame {
         return true;
     }
 
-    // public static void main(String[] args) {
-    //     AutoScheduler AutoScheduler = new AutoScheduler();
-    //     AutoScheduler.pack();
-    //     AutoScheduler.setVisible(true);
-    //   }
 }
